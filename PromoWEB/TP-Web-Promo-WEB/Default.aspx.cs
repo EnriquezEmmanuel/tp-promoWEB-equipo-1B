afde5f5f3d9d37554b60398f37c7f220668ab225
@Emma6891 <enriquez.eml@gmail.com>
2025-05-08 19:41:50 -0300

    Corrección en la consulta VaucherNegocio

--- PromoWEB/TP-Web-Promo-WEB/Default.aspx.cs
+++ PromoWEB/TP-Web-Promo-WEB/Default.aspx.cs
@@ -19,19 +19,38 @@ namespace TP_Web_Promo_WEB
 
         protected void btnValidar_Click(object sender, EventArgs e)
         {
+
+            Image1.Style["opacity"] = "1";
+
+            VaucherNegocio ListaVauchers = new VaucherNegocio();
+            bool validacion = false;
+
+            foreach (Vaucher item in ListaVauchers.lista())
+            {
+                if (item.Codigo == txtBoxVoucher.Text)
+                {
+                    validacion = true;
+                    Session.Add("CodVaucher", item.Codigo);
+                    Session.Add("FechaCanje", item.FechaCanje);
+                    Response.Redirect("ListadoProducto.aspx", false);
+                }
+            }
+            Image1.Style["opacity"] = "0";
+            if (!validacion) lblMensaje.Text = "Vaucher inexistente";
+
+            /*
             Image1.Style["opacity"]="1"; 
             try
             {
                 VaucherNegocio ListaVauchers = new VaucherNegocio();
                 bool validacion = false;
 
-                foreach(var item in ListaVauchers.lista())
+                foreach(Vaucher item in ListaVauchers.lista())
                 {
                     if (item.Codigo == txtBoxVoucher.Text)
                     {
                         validacion = true;
                         Session.Add("CodVaucher", item.Codigo);
-                        Session.Add("IdCliente", item.IdCliente);
                         Response.Redirect("ListadoProducto.aspx", false);
                     }
                 }
@@ -45,11 +64,11 @@ namespace TP_Web_Promo_WEB
                 //ClientScript.RegisterStartupScript(this.GetType(), "MensajeAlerta", Alerta, true);
 
                 Image1.Style["opacity"] = "0";
-                lblMensaje.Text = "alert('Hubo un error inesperado. Vuelva a intentar mas tarde.');";
+                lblMensaje.Text = "Hubo un error inesperado. Vuelva a intentar mas tarde.";
 
-                //throw ex;
+                throw ex;
             }
-
+            */
 
 
         }
